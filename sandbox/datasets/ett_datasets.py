added tests for subdatasets + removed passes and added correct normalization.

sandbox/datasets/ett_datasets.py
@@ -3,7 +3,11 @@ from torch.utils.data import Dataset
 from pathlib import Path
 import pandas as pd
 
-from typing import Union, DictConfig
+from typing import Union, Dict
+from omegaconf import DictConfig
+
+VALID_SUBDATASETS = {"h1", "h2", "m1", "m2"}
+
 
 class ETTDataset(Dataset):
     """
@@ -44,35 +48,74 @@ class ETTDataset(Dataset):
 
     def __init__(self, subdataset: str = "h1", 
                  normalization_type: str = "none",
-                 normalization_params: Union[dict, DictConfig]=None):
-        super(ETTDataset, self).__init__()
-        path_to_data = Path(f"data/ETT/ETT{subdataset}.csv")
-        self.data = pd.read_csv(path_to_data)
-        self._form_timestamp()
-        self.data = self.data.drop(columns=["date"])
-        self.normalization_params = self._normalize_data(normalization_type)
+                 normalization_params: Union[Dict, DictConfig]=None):
         
+        super(ETTDataset, self).__init__()
+
+        # check if subdataset is valid
+        if subdataset not in VALID_SUBDATASETS:
+            raise ValueError(f"Invalid subdataset {subdataset}. Must be one of {VALID_SUBDATASETS}")
+        
+
+
+        path_to_data = Path(f"data/ETT/ETT{subdataset}.csv")
+
+        if not path_to_data.exists():
+            raise FileNotFoundError(f"Data file {path_to_data} not found. Please make sure the ETT dataset is downloaded and placed in the correct directory.")
+
+        self.data = pd.read_csv(path_to_data)
+        self.timestamp = self._form_timestamp()
+        self.target = self.data["OT"]
+
+        self.data = self.data.drop(columns=["date", "OT"])
+        self._normalize_data(normalization_type, normalization_params)
+
         # make torch tensors
         self.timestamp = torch.tensor(self.timestamp, dtype=torch.float32)
         self.data = torch.tensor(self.data.values, dtype=torch.float32)
         self.target = torch.tensor(self.target.values, dtype=torch.float32)
+
     def _form_timestamp(self):
         """Convert the 'date' column to integer timestamps (seconds)."""
         # Convert to pandas datetime, then to integer nanoseconds and to seconds
         timestamps = pd.to_datetime(self.data["date"])
         # Convert to nanoseconds (int64) and then to seconds
-        self.timestamp = (timestamps.astype('int64') // 10**9).to_numpy()
+        timestamp = (timestamps.astype('int64') // 10**9).to_numpy()
+        return timestamp
 
-    def _normalize_data(self, normalization_type: str):
-        if normalization_type not in ["minmax", "none"]:
-            raise NotImplementedError("The normalization type is not implemented yet")
-        if normalization_type == "minmax":
-            self.data = (self.data - self.data.min()) / (self.data.max() - self.data.min())
-            self.target = (self.target - self.target.min()) / (self.target.max() - self.target.min())
-            self.timestamp = (self.timestamp - self.timestamp.min()) / (self.timestamp.max() - self.timestamp.min())
-            self.norma
-        elif normalization_type == "none":
-            return
+    def _normalize_data(self, normalization_type: str, normalization_params: Union[Dict, DictConfig]) -> None:
+        
+        # check if normalization type is supported
+        if normalization_type not in ["minmax", "standard", "none"]:
+            raise NotImplementedError(f"The normalization type {normalization_type} is not implemented yet")
+        
+        # check if normalization params are provided, if so use them, otherwise compute from data
+        
+        # the logics behind this block is that we want to be able to use the same normalization params for train/val/test splits, which are computed from the train split, and passed to the val/test splits via the config
+        if normalization_params is not None:
+            if normalization_type == "minmax":
+                self.data = (self.data - normalization_params["data_min"]) / (normalization_params["data_max"] - normalization_params["data_min"])
+                self.target = (self.target - normalization_params["target_min"]) / (normalization_params["target_max"] - normalization_params["target_min"])
+                self.timestamp = (self.timestamp - normalization_params["timestamp_min"]) / (normalization_params["timestamp_max"] - normalization_params["timestamp_min"])
+                return 
+            elif normalization_type == "standard":
+                self.data = (self.data - normalization_params["data_mean"]) / normalization_params["data_std"]
+                self.target = (self.target - normalization_params["target_mean"]) / normalization_params["target_std"]
+                self.timestamp = (self.timestamp - normalization_params["timestamp_mean"]) / normalization_params["timestamp_std"]
+                return
+        else:
+
+            if normalization_type == "minmax":
+                self.data = (self.data - self.data.min()) / (self.data.max() - self.data.min())
+                self.target = (self.target - self.target.min()) / (self.target.max() - self.target.min())
+                self.timestamp = (self.timestamp - self.timestamp.min()) / (self.timestamp.max() - self.timestamp.min())
+                return
+            elif normalization_type == "standard":
+                self.data = (self.data - self.data.mean()) / self.data.std()
+                self.target = (self.target - self.target.mean()) / self.target.std()
+                self.timestamp = (self.timestamp - self.timestamp.mean()) / self.timestamp.std()
+                return
+        return
 
     def __len__(self):
         return len(self.data)
@@ -83,10 +126,12 @@ class ETTDataset(Dataset):
 
 if __name__ == "__main__":
     subdataset = "h1"
+    print("Testing ETTDataset with subdataset:", subdataset)
     dataset = ETTDataset(subdataset="h1")
     print(len(dataset))
     print(dataset[0])
     print(dataset[1])
+
     for elem in dataset:
         for subelem in elem:
             assert ~(torch.isnan(subelem)).any(), f"NaN found in ETT{subdataset} dataset"